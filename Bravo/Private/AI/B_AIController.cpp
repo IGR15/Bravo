d0e5dc549ff1df0cb9bd9d731698400d76e29208
IGR15 <omarswiti10@gmail.com>
2025-12-14 18:41:44 +0200

    project bravo

---
+++ Bravo/Private/AI/B_AIController.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/B_AIController.h"
+
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+// Sets default values
+AB_AIController::AB_AIController()
+{
+	Blackboard=CreateDefaultSubobject<UBlackboardComponent>("BlackboardComponent");
+	check(Blackboard);
+	BehaviorTreeComponent=CreateDefaultSubobject<UBehaviorTreeComponent>("BehaviorTreeComponent");
+	check(BehaviorTreeComponent);
+}
+
+
+
+
---
+++ Bravo/Public/AI/B_AIController.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "B_AIController.generated.h"
+
+
+class UBlackboardComponent;
+class UBehaviorTreeComponent;
+UCLASS()
+class BRAVO_API AB_AIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	
+	AB_AIController();
+
+protected:
+	UPROPERTY()
+	TObjectPtr<UBehaviorTreeComponent>BehaviorTreeComponent;
+};
