a383869f6f70441b1e0a652ac1255a30468704af
IGR15 <omarswiti10@gmail.com>
2026-01-03 20:22:07 +0200

    Playerstate

---
+++ Bravo/Public/Player/B_PlayerState.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/PlayerState.h"
+#include "B_PlayerState.generated.h"
+
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+/**
+ * 
+ */
+UCLASS()
+class BRAVO_API AB_PlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+};
