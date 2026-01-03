3135401f2c68730cc7d7f4ad023422ab2a890673
IGR15 <omarswiti10@gmail.com>
2026-01-03 20:11:49 +0200

    Attributs

--- Bravo/Public/GAS/B_AttributeSet.h
+++ Bravo/Public/GAS/B_AttributeSet.h
@@ -4,13 +4,65 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "B_AttributeSet.generated.h"
 
 /**
  * 
  */
+#define ATTRIBUTE_ACCESSORS_BASIC(ClassName,PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAttributeInitialized);
+
 UCLASS()
 class BRAVO_API UB_AttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+	UPROPERTY(BlueprintAssignable)
+	FAttributeInitialized  OnAttributeInitializedDelegate;
+	
+	UPROPERTY(ReplicatedUsing=OnRep_AttributesInitialized)
+	bool bAttributeInitialized=false;
+
+	UFUNCTION()
+	void OnRep_AttributesInitialized();
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=onRep_Health)
+	FGameplayAttributeData Health;
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_Mana)
+	FGameplayAttributeData Mana;
+	
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing=OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue );
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue );
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldValue );
+	
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldValue );
+	
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass,Health);
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass,MaxHealth);
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass,Mana);
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass,MaxMana);
+
 };
